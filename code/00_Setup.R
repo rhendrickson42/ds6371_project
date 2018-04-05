9dff767d3f719b592ae31a783697a0b4a9caa467
vagrant <vagrant@mjk.local.com>
2018-04-05 17:35:53 -0400

    commit changes

--- code/00_Setup.R
+++ code/00_Setup.R
@@ -28,5 +28,5 @@ check.packages <- function(pkg){
     sapply(pkg, require, character.only = TRUE)
 }
 
-packages<-c( "dplyr", "ggplot2", "doBy", "stringr", "reshape2", "gridExtra", "gplots", "randomForest", "caret")
+packages<-c( "dplyr", "ggplot2", "doBy", "stringr", "reshape2", "gridExtra", "gplots")
 check.packages(packages)
